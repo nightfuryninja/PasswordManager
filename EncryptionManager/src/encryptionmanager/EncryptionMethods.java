669ce4d8318e21abb92fed0077e4cb50e35dcb7a
Scriptle <44384393+Scriptle@users.noreply.github.com>
2019-01-27 02:51:34 +0000

    Update EncryptionMethods.java

--- EncryptionManager/src/encryptionmanager/EncryptionMethods.java
+++ EncryptionManager/src/encryptionmanager/EncryptionMethods.java
@@ -3,6 +3,10 @@ package encryptionmanager;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
@@ -55,6 +59,37 @@ public class EncryptionMethods {
         } catch (InvalidKeySpecException ex) {
             System.out.println("Sorry, that is an invalid key specification.");
             return null;
+        }
+    }
+    
+    public static void connect() {        
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:test.db";
+            String sql = "CREATE TABLE IF NOT EXISTS passwords (id integer PRIMARY KEY,website VARCHAR2,username BINARY,password BINARY)";
+            //String sql2 = "INSERT INTO passwords(website,username,password) VALUES(?,?,?)";
+            conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            stmt.execute(sql);
+            /*
+            PreparedStatement pstmt = conn.prepareStatement(sql2);
+            pstmt.setString(1, "Test");
+            pstmt.setString(2, "Test");
+            pstmt.setString(3, "Test");
+            pstmt.executeUpdate();
+            */
+            
+            System.out.println("Connection Sucessful");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
         }
     }
     
