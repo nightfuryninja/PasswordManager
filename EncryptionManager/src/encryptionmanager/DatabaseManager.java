a0f75992a65d6b1eff314845d2670933f006371b
Harry Ramsey <nightfurryninja@gmail.com>
2019-01-28 00:56:33 +0000

    Update DatabaseManager.java
    DatabaseManager.java will now check to see if the table has been created and will create one if it doesn't already exist.

--- EncryptionManager/src/encryptionmanager/DatabaseManager.java
+++ EncryptionManager/src/encryptionmanager/DatabaseManager.java
@@ -10,14 +10,31 @@ import java.util.logging.Logger;
 
 public class DatabaseManager {
     
-    private Connection conn = null;
+    private Connection conn;
 
     public DatabaseManager(String url) {
+        connectToDatabase();
+    }
+    
+    //Connects to the database.
+    private void connectToDatabase() {
         try {
-            conn = DriverManager.getConnection("jdbc:sqlite:" + url);
-            System.out.println("Connection Sucessful.");
+            String url = "jdbc:sqlite:test.db";
+            String sql = "CREATE TABLE IF NOT EXISTS accounts (id integer PRIMARY KEY,website VARCHAR2,username BINARY,password BINARY)";
+            conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            stmt.execute(sql);
+            System.out.println("Connection Sucessful");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
         }
     }
     
@@ -56,32 +73,10 @@ public class DatabaseManager {
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
-    }   
-
-    //Connects to the database.
-    private void ConnectToDatabase() {
-        try {
-            String url = "jdbc:sqlite:test.db";
-            String sql = "CREATE TABLE IF NOT EXISTS accounts (id integer PRIMARY KEY,website VARCHAR2,username BINARY,password BINARY)";
-            conn = DriverManager.getConnection(url);
-            Statement stmt = conn.createStatement();
-            stmt.execute(sql);
-            System.out.println("Connection Sucessful");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
     }
 
     //Inserts a new row of account data into the database.
-    public void InsertRow(String[] args) {
+    public void insertRow(String[] args) {
         String command = String.format("INSERT INTO accounts(website, username, password) "
                 + "VALUES({0}, {1}, {2})", args);
         try {
@@ -94,7 +89,7 @@ public class DatabaseManager {
     }
 
     //Updates an existing row (using ID) in the database.
-    public void UpdateRow(String[] args) {
+    public void updateRow(String[] args) {
         String command = String.format("UPDATE accounts SET {0} = {1} WHERE id  = {3};", args);
         try {
             Statement statement = conn.createStatement();
