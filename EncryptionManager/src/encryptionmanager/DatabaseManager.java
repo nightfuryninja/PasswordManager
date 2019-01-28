308d57f527cd2670923de0913d2d2db5c27bc373
Scriptle <44384393+Scriptle@users.noreply.github.com>
2019-01-28 01:30:40 +0000

    Update DatabaseManager.java

--- EncryptionManager/src/encryptionmanager/DatabaseManager.java
+++ EncryptionManager/src/encryptionmanager/DatabaseManager.java
@@ -5,74 +5,17 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-public class DatabaseManager {
-    
-    private Connection conn = null;
-
-    public DatabaseManager(String url) {
-        try {
-            conn = DriverManager.getConnection("jdbc:sqlite:" + url);
-            System.out.println("Connection Sucessful.");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-    
-    public void execute(String sql) {
-        try {
-            Statement stmt = conn.createStatement();
-            stmt.execute(sql);
-        } catch (SQLException ex) {
-            System.out.println(ex);
-        }
-    }
-    
-    public PreparedStatement createPreparedStatement(String sql) {
-        PreparedStatement pstmt = null;
-        try {
-            pstmt = conn.prepareStatement(sql);
-        } catch (SQLException ex) {
-            System.out.println(ex);
-        }
-        return pstmt;
-    }
-    
-    public void executePreparedStatement(PreparedStatement pstmt) {
-        try {
-            pstmt.executeUpdate();
-        } catch (SQLException ex) {
-            System.out.println(ex);;
-        }
-    }
-
-    public void close() {
-        try {
-            if (conn != null) {
-                conn.close();
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }   
-    
-}
-=======
-import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DatabaseManager {
 
     private Connection conn;
 
-    public DatabaseManager() {
-        ConnectToDatabase();
+    public DatabaseManager(String url) {
+        connectToDatabase();
     }
 
     //Connects to the database.
-    private void ConnectToDatabase() {
+    private void connectToDatabase() {
         try {
             String url = "jdbc:sqlite:test.db";
             String sql = "CREATE TABLE IF NOT EXISTS accounts (id integer PRIMARY KEY,website VARCHAR2,username BINARY,password BINARY)";
@@ -93,8 +36,45 @@ public class DatabaseManager {
         }
     }
 
+    public void execute(String sql) {
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.execute(sql);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public PreparedStatement createPreparedStatement(String sql) {
+        PreparedStatement pstmt = null;
+        try {
+            pstmt = conn.prepareStatement(sql);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return pstmt;
+    }
+
+    public void executePreparedStatement(PreparedStatement pstmt) {
+        try {
+            pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex);;
+        }
+    }
+
+    public void close() {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
     //Inserts a new row of account data into the database.
-    public void InsertRow(String[] args) {
+    public void insertRow(String[] args) {
         String command = String.format("INSERT INTO accounts(website, username, password) "
                 + "VALUES({0}, {1}, {2})", args);
         try {
@@ -107,7 +87,7 @@ public class DatabaseManager {
     }
 
     //Updates an existing row (using ID) in the database.
-    public void UpdateRow(String[] args) {
+    public void updateRow(String[] args) {
         String command = String.format("UPDATE accounts SET {0} = {1} WHERE id  = {3};", args);
         try {
             Statement statement = conn.createStatement();
