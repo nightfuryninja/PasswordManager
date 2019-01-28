3255bfbd4102b404fbd85ee4a172f20b7a787752
Harry Ramsey <nightfurryninja@gmail.com>
2019-01-28 01:22:02 +0000

    Changed Around a Few Files.

--- EncryptionManager/src/encryptionmanager/EncryptionManager.java
+++ EncryptionManager/src/encryptionmanager/EncryptionManager.java
@@ -10,4 +10,15 @@ public class EncryptionManager {
     //3) Create a pretty GUI that looks all fancy and modern.
     public static Scanner input = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        Thread guiThread = new Thread() {
+            @Override
+            public void run() {
+                GUI gui = new GUI();
+                gui.setVisible(true);
+            }
+        };
+        guiThread.run();
+    }
+
 }
