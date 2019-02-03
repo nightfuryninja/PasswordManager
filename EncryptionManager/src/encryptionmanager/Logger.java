2ed07422b2dd79a2b008b21ed2b330eb365c7009
Harry Ramsey <nightfurryninja@gmail.com>
2019-02-03 23:36:47 +0000

    Added Logger.java class which will handle any errors and crash reports the user might have.

---
+++ EncryptionManager/src/encryptionmanager/Logger.java
@@ -0,0 +1,18 @@
+package encryptionmanager;
+
+public class Logger {
+    
+    public void debug(String debugData){
+        System.out.println(debugData);
+        
+    }
+    
+    public void error(String Data, Exception e){
+        System.out.println(errorData);
+    }
+    
+    private void writeToLog(){
+        
+    }
+    
+}
